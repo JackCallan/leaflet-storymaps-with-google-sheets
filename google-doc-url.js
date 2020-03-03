4591b16b653d9a32329357e1df5e8929ab7ed889
JackCallan <61739137+JackCallan@users.noreply.github.com>
2020-03-03 15:07:55 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1vd1GlngiHeeYKsfSWz5wAwZ2PHm-gPA_9UktKu8N9XA/edit#gid=0';
